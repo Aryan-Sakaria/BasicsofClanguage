315c2b0c64b07523d5955e7053968b0d9acd36bf
Aryan-Sakaria <76280290+Aryan-Sakaria@users.noreply.github.com>
2024-03-29 12:31:40 +0530

    Create addtwonumbers.c

---
+++ addtwonumbers.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main(){
+    int a,b,sum;
+    printf("Enter the both numbers: ");
+    scanf("%d %d",&a,&b);
+    sum=a+b;
+    printf("The sum of %d and %d is %d", a,b,sum);
+    }
